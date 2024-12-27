04e344e44915d78e3eb626c3c331e0141b3bfc77
Shivang <shivangptl099@gmail.com>
2024-12-27 18:29:21 +0530

    minor change

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import { useState } from "react";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
