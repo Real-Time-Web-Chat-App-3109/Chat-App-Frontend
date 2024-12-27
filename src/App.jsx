dcceff24cfe3c14c8fc27dee881e118555b4feca
ShivPatel031 <patelshiv3123@gmail.com>
2024-12-27 11:57:59 +0530

    made shiv branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-blue-800 text-center">welcome to chat app.</h1>
+      <h1 className="text-blue-800 text-center text-[100px]">welcome to chat app.</h1>
     </>
   )
 }
